17cfe1cc84fad73145e78844ed5f1ac1209e24b2
lgcovizzi <lgcovizzi@gmail.com>
2025-02-26 18:06:09 -0300

    feat:add SayHello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lgcovizzi/toolsToGo
+
+go 1.24.0
---
+++ utils.go
@@ -0,0 +1,8 @@
+package toolsToGo
+
+import "fmt"
+
+// SayHello imprime uma saudação
+func SayHello(name string) {
+	fmt.Printf("Olá, %s!\n", name)
+}
