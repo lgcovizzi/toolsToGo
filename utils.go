3b3e3d2454984d623554cbd838e5676df24a40b5
lgcovizzi <lgcovizzi@gmail.com>
2025-02-26 18:43:52 -0300

    feat: add GetProjectRoot

--- utils.go
+++ utils.go
@@ -1,8 +1,33 @@
 package toolsToGo
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 // SayHello imprime uma saudação
 func SayHello(name string) {
 	fmt.Printf("Olá, %s!\n", name)
+}
+
+func GetProjectRoot() (string, error) {
+	// Obtém o diretório atual
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	// Navega até a raiz do projeto
+	for {
+		if _, err := os.Stat(filepath.Join(dir, "go.mod")); err == nil {
+			return dir, nil
+		}
+		parentDir := filepath.Dir(dir)
+		if parentDir == dir {
+			break
+		}
+		dir = parentDir
+	}
+	return "", fmt.Errorf("raiz do projeto não encontrada")
 }
